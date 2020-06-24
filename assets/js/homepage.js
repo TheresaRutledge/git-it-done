2eab60e17d79fe6824917c38bdb165460e96585d
Theresa Rutledge <t.roserutledge@gmail.com>
2020-06-24 11:51:20 -0700

    search repos by language

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,20 +2,21 @@ const userFormEl = document.querySelector('#user-form');
 const nameInputEl = document.querySelector('#username');
 const repoContainerEl = document.querySelector('#repos-container');
 const repoSeachTerm = document.querySelector('#repo-search-term');
+const languageButtonsEl = document.querySelector('#language-buttons');
 
 const getUserRepos = (user) => {
     fetch(`https://api.github.com/users/${user}/repos`)
         .then(function (response) {
-            if(response.ok){
-            response.json()
-                .then(function (data) {
-                    displayRepos(data, user)
-                })
+            if (response.ok) {
+                response.json()
+                    .then(function (data) {
+                        displayRepos(data, user)
+                    })
             } else {
                 alert(`Error: ${response.statusText}`)
             }
         })
-        .catch(function(error){
+        .catch(function (error) {
             alert('Unable to connect to GitHub');
         })
 
@@ -36,20 +37,18 @@ const displayRepos = (repos, searchTerm) => {
     repoContainerEl.textContent = '';
     repoSeachTerm.textContent = searchTerm;
 
-    if(repos.length === 0){
+    if (repos.length === 0) {
         repoContainerEl.textContent = "No repositories found"
         return;
     }
 
     for (i = 0; i < repos.length; i++) {
         let repoName = `${repos[i].owner.login}/${repos[i].name}`;
-        console.log(repoName);
-        console.log(typeof repoName);
 
         let repoEl = document.createElement('a');
         repoEl.classList = 'list-item flex-row justify-space-between align center';
-        repoEl.setAttribute('href',"./single-repo.html?repo=" +repoName);
-   
+        repoEl.setAttribute('href', "./single-repo.html?repo=" + repoName);
+
 
         let titleEl = document.createElement('span');
         titleEl.textContent = repoName;
@@ -59,7 +58,7 @@ const displayRepos = (repos, searchTerm) => {
         let statusEl = document.createElement('span');
         statusEl.classList = 'flex-row align-center';
 
-        if(repos[i].open_issues_count > 0){
+        if (repos[i].open_issues_count > 0) {
             statusEl.innerHTML = `<i class='fas fa-times status-icon icon-danger'></i> ${repos[i].open_issues_count} issues(s)`;
         } else {
             statusEl.innerHTML = `<i class='fas fa-check-square status-icon icon-success'></i>`;
@@ -72,5 +71,28 @@ const displayRepos = (repos, searchTerm) => {
 
 userFormEl.addEventListener('submit', formSubmitHandler);
 
-// getUserRepos('TheresaRutledge');
+const getFeaturedRepos = (language) => {
+    var apiUrl = `https://api.github.com/search/repositories?q=${language}+is:featured&sort=help-wanted-issues`;
+    fetch(apiUrl)
+        .then(function (response) {
+            if (response.ok) {
+                response.json()
+                    .then(function (data) {
+                        displayRepos(data.items, language)
+                    })
+            } else {
+                alert(`Error: ${response.statusText}`);
+            }
+        })
+}
+const buttonClickHandler =(event)=>{
+    let language = event.target.getAttribute('data-language');
+    if(language){
+    getFeaturedRepos(language);
+    repoContainerEl.textContent = '';
+    }
+}
+
+
+languageButtonsEl.addEventListener('click',buttonClickHandler);
 
